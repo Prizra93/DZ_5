38337b247e64d4b190a14a2769b5312769735627
AksX_kv36 <AksX_kv36@student.itstep.org>
2022-02-20 19:01:27 +0200

    Исправление

--- Header.h
+++ Header.h
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<math.h>
-#include<iomanip>
+#include<stdlib.h>
+#include<time.h>
 
 using namespace std;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include "insertSort.h"
 
-int main() {
-   
+int main()
+{
+    int const length = 20;
+    int array[length];
+    srand(time(NULL));
+    for (int i = 0; i < length; i++) {
+        array[i] = rand() % 40 - 10;
+        cout << array[i] << " ";
+    }
+    cout << '\n';
+    insertSort(array, length);
+    cout << '\n';
     return 0;
 }
